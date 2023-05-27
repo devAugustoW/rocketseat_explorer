137bb42cc702fe834e73f91b9938560c9c49dc9c
Augusto <128865750+devAugustoW@users.noreply.github.com>
2023-05-26 22:18:31 -0300

    Update jogo da adivinhação

--- stage_05/Jogo_da_adivinhacao/main.js
+++ stage_05/Jogo_da_adivinhacao/main.js
@@ -1,15 +1,52 @@
-const randonNumber = Math.round(Math.random() * 10)
-let tentativas = 1
+//Variáveis
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+let guessNumber = document.querySelector('#inputNumber')
+const btnStartAgain = document.querySelector('.btnStartAgain')
 
-function teste() {
-    
 
-    let guessNumber = document.querySelector('#inputNumber')
+let randonNumber = Math.round(Math.random() * 10)
+let tentativas = 0
 
-    if (Number(guessNumber) == randonNumber) {
+console.log('Randon Number: '+ randonNumber)
 
-    }
+
+function btnTry() {
 
     tentativas++
-    console.log(tentativas)
+    
+    if (Number(guessNumber.value) == randonNumber) {
+        
+        const finalFrase = document.querySelector('#finalFrase')
+
+        toggleScreen()
+
+        finalFrase.innerText = 'Acertou em '+ tentativas+ ' tentativa(s)!'
+
+    }else {
+
+        document.querySelector('#inputNumber').value = ''
+        guessNumber.focus()
+    }
+}  
+
+
+btnStartAgain.addEventListener('click', starAgain)
+
+function starAgain() {
+
+    tentativas = 0
+    randonNumber = Math.round(Math.random() * 10)
+
+    toggleScreen()
+
+    document.querySelector('#inputNumber').value = ''
+    guessNumber.focus()
+
+    console.log('Randon Number: '+ randonNumber)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
 }
