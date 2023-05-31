39a9b9f7c516aef961f00c106fbbadd70f35c5f0
Augusto <128865750+devAugustoW@users.noreply.github.com>
2023-05-31 16:37:12 -0300

    Update biscoito da sorte
    Criação do carrossel de frases

--- stage_05/Biscoito_da_sorte/main.js
+++ stage_05/Biscoito_da_sorte/main.js
@@ -4,15 +4,38 @@ const cookie = document.querySelector('#cookie')
 const lucky = document.querySelector('.lucky')
 const btnTryAgain = document.querySelector('#btnTryAgain')
 
+const luckyPhrase = document.querySelector('#luckyPhrase')
+
+
 cookie.addEventListener('click', function() {
     toggleClass()
+
+    chooseRandomPhrase()
 })
 
 btnTryAgain.addEventListener('click', function() {
     toggleClass()
 })
 
+
 function toggleClass() {
     home.classList.toggle('hide')
     lucky.classList.toggle('hide')
 }
+
+
+const phrases = 
+[
+    "Acredite em si mesmo e tudo será possível.",
+    "O único limite é aquele que você impõe a si mesmo.",
+    "Persista, pois o caminho para o sucesso é construído passo a passo.",
+    "Seja a mudança que você deseja ver no mundo.",
+    "Nunca desista, pois as maiores conquistas vêm para aqueles que perseveram.",
+    "Cada novo dia é uma oportunidade para recomeçar e alcançar seus objetivos."
+]
+
+function chooseRandomPhrase() {
+     const randomNumber = Math.floor(Math.random() * phrases.length) 
+     luckyPhrase.innerHTML = phrases[randomNumber]
+}
+
