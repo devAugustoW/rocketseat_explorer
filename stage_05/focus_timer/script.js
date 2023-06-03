80a1e65d626e909d61093505d4966e9b54e09692
Augusto <128865750+devAugustoW@users.noreply.github.com>
2023-06-03 01:05:25 -0300

    Up date

--- stage_05/focus_timer/script.js
+++ stage_05/focus_timer/script.js
@@ -1,30 +1,56 @@
 // VARIÁVEIS
-// Obter o elemento HTML que exibe os minutos
-let inputMinutes = document.querySelector('.minutes')
-// Obter o elemento HTML que exibe os segundos
-let inputSeconds = document.querySelector('.seconds')
+// Obter o elemento HTML que exibe os minutos e segundos
+let minutesHTML = document.querySelector('.minutes')
+let secondsHTML = document.querySelector('.seconds')
 
-const btnPlay = document.querySelector('.play')
-const btnPause = document.querySelector('.pause')
-const btnStop = document.querySelector('.stop')
-const btnSet = document.querySelector('.btnSet')
+let btnPlay = document.querySelector('.play')
+let btnPause = document.querySelector('.pause')
+let btnStop = document.querySelector('.stop')
+let btnSet = document.querySelector('.btnSet')
+let btnSoundOn = document.querySelector('.sound-on')
+let btnSoundOff = document.querySelector('.sound-off')
 
-const btnSoundOn = document.querySelector('.sound-on')
-const btnSoundOff = document.querySelector('.sound-off')
-
-
+let seconds
 let minutes
+let internvalID
 
-// EVENTOS E FUNÇÕES
 
+function callCountdown() {
 
-function countdown() {
-    setTimeout(function() {
+    //FUNÇÃO EXECUTADA A CADA 1000ms
+    internvalID = setInterval(() => {
+
+        //TRANSFOMRAR EM NÚMERO
+        minutes = Number(minutes)
+        seconds = Number(seconds)
+
+        //MANIPULAR OS NÚMEROS
+        if (minutes == 0 && seconds == 0){
+            return
+        } 
         
+        if (seconds == 0) {
+            minutes--
+            seconds = 11
+        } else {
+            seconds--
+        }
 
-    }, 1000) 
+        //TRANSFORMAR EM STRING E IMPRIMIR OS "NÚMEROS"
+        upDateDisplay()
+        
+     }, 1000)
 }
 
+function upDateDisplay() {
+    minutesHTML.textContent = String(minutes).padStart(2, '0')
+    secondsHTML.textContent = String(seconds).padStart(2, '0')
+}
+
+function resetDisplay() {
+    minutesHTML.textContent = String('00')
+    secondsHTML.textContent = String('00')
+}
 
 btnPlay.addEventListener('click', function() {
     btnPlay.classList.add('hide')
@@ -32,37 +58,50 @@ btnPlay.addEventListener('click', function() {
     btnSet.classList.add('hide')
     btnStop.classList.remove('hide')
 
-    countdown() 
-    
+    callCountdown();
 })
+
 
 btnPause.addEventListener('click', function() {
     btnPause.classList.add('hide')
     btnPlay.classList.remove('hide')
+
+    clearInterval(internvalID)
 })
 
 btnStop.addEventListener('click', function() {
-    btnPause.classList.add('hide')
-    btnPlay.classList.remove('hide')
     btnStop.classList.add('hide')
+    btnPause.classList.add('hide')
     btnSet.classList.remove('hide')
+    btnPlay.classList.remove('hide')
+
+    clearInterval(internvalID)
+
+    resetDisplay()
 })
+
 
 btnSet.addEventListener('click', function() {
-    let minutes = Number(prompt('Quantos minutos?'))
-    inputMinutes.innerHTML = minutes
+    let newMinutes = prompt('Digite alguma coisa')
+    if (!newMinutes) {
+        
+        return
+    }
+
+    if (minutes === null) {
+        resetDisplay()
+    }
+
+    seconds = 0
+    console.log(typeof(seconds))
+    
+    upDateDisplay()
+})  
 
 
-    console.log('Minutes: '+ minutes)
-    console.log('Type of minutes: '+ typeof(minutes))
-})
 
-btnSoundOff.addEventListener('click', function() {
-    btnSoundOff.classList.toggle('hide')
-    btnSoundOn.classList.toggle('hide')
-})
 
-btnSoundOn.addEventListener('click', function() {
-    btnSoundOn.classList.toggle('hide')
-    btnSoundOff.classList.toggle('hide')
-})
+
+
+
+
