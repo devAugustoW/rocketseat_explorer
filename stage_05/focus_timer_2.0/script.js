0a8cfe702fb8186fafca429ae6b26de579167d1e
Augusto <128865750+devAugustoW@users.noreply.github.com>
2023-06-16 15:22:47 -0300

    teste focus timer 2.0

--- stage_05/focus_timer_2.0/script.js
+++ stage_05/focus_timer_2.0/script.js
@@ -1,3 +1,61 @@
+// Falta ver bug do btnPlay quando clicado mais de 1 vez
+// Falta ver bug dos minutos que só add ou diminui com a contagem em curso
+// Falta ver bug do btnMinus quando subtrai tempo fica menor que 00:00
+// Falta trocar cores dos card buttons quando clicados
+
+
+
+import { cafeteriaSound, chuvaSound, florestaSound, lareiraSound } from "./sounds.js"
+
+const btnTree = document.querySelector('.tree')
+const btnRain = document.querySelector('.rain')
+const btnShop = document.querySelector('.shop')
+const btnFire = document.querySelector('.fire')
+
+btnTree.addEventListener('click', function(){
+    console.log('entrei no card tree')
+
+    cafeteriaSound.pause()
+    chuvaSound.pause()
+    lareiraSound.pause()
+    florestaSound.play()
+
+})
+
+btnRain.addEventListener('click', function(){
+    console.log('entrei no card rain')
+
+    cafeteriaSound.pause()
+    lareiraSound.pause()
+    florestaSound.pause()
+    chuvaSound.play()
+
+})
+
+btnShop.addEventListener('click', function(){
+    console.log('entrei no card shop')
+
+    lareiraSound.pause()
+    florestaSound.pause()
+    chuvaSound.pause()
+    cafeteriaSound.play()
+
+})
+
+btnFire.addEventListener('click', function(){
+    console.log('entrei no card fire')
+
+    cafeteriaSound.pause()
+    chuvaSound.pause()
+    florestaSound.pause()
+    lareiraSound.play()
+
+})
+
+
+
+
+
 let minutesHTML = document.querySelector('.minutes')
 let secondsHTML = document.querySelector('.seconds')
 
@@ -6,4 +64,57 @@ const btnStop = document.querySelector('.stop')
 const btnPlus = document.querySelector('.plusMinutes')
 const btnMinus = document.querySelector('.minusMinutes')
 
-minutesHTML.textContent = 'A0'
+let minutes = 25
+let seconds = 0
+let internvalID
+
+
+function upDateDisplay() {
+    minutesHTML.textContent = String(minutes).padStart(2, '0')
+    secondsHTML.textContent = String(seconds).padStart(2, '0')
+}
+
+
+function startCountDown() {
+
+    internvalID = setInterval(() => {
+        console.log('entrei')
+        
+        if(minutes <= 0 && seconds == 0) {
+            minutesHTML.textContent = String('00')
+            secondsHTML.textContent = String('00')
+            return
+        }
+
+        if (seconds == 0) {
+            seconds = 5
+            minutes--
+        } else {
+            seconds--
+        }
+
+        upDateDisplay()
+    }, 1000)
+
+}
+
+btnPlay.addEventListener('click', function(){
+    startCountDown()
+})
+
+btnStop.addEventListener('click', function() {
+    clearInterval(internvalID)
+})
+
+btnPlus.addEventListener('click', function(){
+    minutes += 5
+})
+
+btnMinus.addEventListener('click', function(){
+    if(minutes <= 0 && seconds == 0) {
+        minutesHTML.textContent = String('00')
+        secondsHTML.textContent = String('00')
+        return
+    }
+    minutes -= 5
+})
---
+++ stage_05/focus_timer_2.0/sounds.js
@@ -0,0 +1,4 @@
+export const cafeteriaSound = new Audio("./audio/Cafeteria.wav")
+export const chuvaSound  = new Audio("./audio/Chuva.wav")
+export const florestaSound = new Audio("./audio/Floresta.wav")
+export const lareiraSound = new Audio("./audio/Lareira.wav")
